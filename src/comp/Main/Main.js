db3b3f026040a1390b11a82d6795e35376cedc5a
Whatruska <vitdub@inbox.ru>
2020-05-16 21:03:24 +0600

    section bug fixed

--- src/comp/Main/Main.js
+++ src/comp/Main/Main.js
@@ -10,6 +10,7 @@ import classes from "./Main.module.css"
 import FirstSection from "./FirstSection/FirstSection";
 import DescSection from "./DescSection/DescSection";
 import VideoSection from "./VideoSection/VideoSection";
+import Infographics from "./Infographics/Infographics";
 
 let Main = () => {
     let lorem = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
@@ -27,6 +28,7 @@ let Main = () => {
                 link={"/people"}
                 linkDesc={"Читать подробнее"}
             />
+            <Infographics/>
             <Section
                 id={"science"}
                 src={science}
@@ -34,7 +36,6 @@ let Main = () => {
                 heading={"Научная  деятельность в казанском университете в годы войны"}
                 desc={lorem}
                 link={"/science"}
-                height={"964px"}
                 linkDesc={"Читать подробнее..."}
             />
             <Section
--- src/comp/Main/Section/Section.js
+++ src/comp/Main/Section/Section.js
@@ -3,11 +3,13 @@ import classes from "./Section.modules.css";
 import {NavLink} from "react-router-dom";
 
 let Section = (props) => {
+    let sectionRef = React.createRef();
+    let setHeight = (e) => {
+
+    }
     return(
-        <section className={classes.Section} id={props.id} style={{
-            height : props.height
-        }}>
-            <img src={props.src} alt={props.alt}/>
+        <section className={classes.Section} id={props.id} ref={sectionRef}>
+            <img src={props.src} alt={props.alt} onLoad={setHeight}/>
             <div className={classes.sectionContent}>
                 <h2 className={classes.heading}>{props.heading}</h2>
                 <span className={classes.desc}>{props.desc}</span>
