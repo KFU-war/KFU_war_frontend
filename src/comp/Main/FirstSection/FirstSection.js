d13872e1c31fc22d0699c86b34ff35b3bddb4048
Whatruska <vitdub@inbox.ru>
2020-05-16 16:14:40 +0600

    desc section complete

---
+++ src/comp/Main/FirstSection/FirstSection.js
@@ -0,0 +1,25 @@
+import classes from "./FirstSection.module.css"
+import main from "../../../assets/img/main.png";
+import leftPart from "../../../assets/img/arrow_left.png";
+import rightPart from "../../../assets/img/arrow_right.png";
+import React from "react";
+
+let FirstSection = (props) => {
+    return(
+        <section className={classes.MainSection} id={"#main"}>
+            <img src={main} alt={"КФУ"}/>
+            <div className={classes.sectionContent}>
+                <span className={classes.years}>1941-1945</span>
+                <h2 className={classes.heading}>Казанский университет в годы войны</h2>
+                <div>
+                    <a href={"#video"}className={classes.arrow}>
+                        <img src={leftPart} className={classes.arrow_part} alt={"Стрелка"}/>
+                        <img src={rightPart} className={classes.arrow_part + " " + classes.rigth_part} alt={"Стрелка"}/>
+                    </a>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default FirstSection;
--- src/comp/Main/Main.js
+++ src/comp/Main/Main.js
@@ -4,29 +4,17 @@ import Feedback from "./Feedback/Feedback";
 
 import science from '../../assets/img/science.png';
 import people from '../../assets/img/people.png';
-import main from "../../assets/img/main.png";
-import leftPart from "../../assets/img/arrow_left.png";
-import rightPart from "../../assets/img/arrow_right.png";
 
 import classes from "./Main.module.css"
+import FirstSection from "./FirstSection/FirstSection";
+import DescSection from "./DescSection/DescSection";
 
 let Main = () => {
     let lorem = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
     return(
         <>
-            <section className={classes.MainSection} id={"#main"}>
-                <img src={main} alt={"КФУ"}/>
-                <div className={classes.sectionContent}>
-                    <span className={classes.years}>1941-1945</span>
-                    <h2 className={classes.heading}>Казанский университет в годы войны</h2>
-                    <div>
-                        <a href={"#video"}className={classes.arrow}>
-                            <img src={leftPart} className={classes.arrow_part} alt={"Стрелка"}/>
-                            <img src={rightPart} className={classes.arrow_part + " " + classes.rigth_part} alt={"Стрелка"}/>
-                        </a>
-                    </div>
-                </div>
-            </section>
+            <FirstSection/>
+            <DescSection/>
             <Section
                 id={"people"}
                 src={people}
