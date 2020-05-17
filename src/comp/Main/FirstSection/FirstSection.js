72ccff688f2b0e3a0cf98c02d71a4a6894454656
Whatruska <vitdub@inbox.ru>
2020-05-17 16:26:41 +0600

    minor-fixes

--- src/comp/Main/FirstSection/FirstSection.js
+++ src/comp/Main/FirstSection/FirstSection.js
@@ -3,8 +3,16 @@ import main from "../../../assets/img/main.png";
 import leftPart from "../../../assets/img/arrow_left.png";
 import rightPart from "../../../assets/img/arrow_right.png";
 import React from "react";
+import $ from "jquery";
 
 let FirstSection = (props) => {
+    let click = (e) => {
+        e.preventDefault();
+        debugger;
+        let id  = e.currentTarget.getAttribute('href');
+        let top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top - 160}, 750);
+    }
     return(
         <section className={classes.MainSection} id={"#main"}>
             <img src={main} alt={"КФУ"}/>
@@ -12,7 +20,7 @@ let FirstSection = (props) => {
                 <span className={classes.years}>1941-1945</span>
                 <h2 className={classes.heading}>Казанский университет в годы войны</h2>
                 <div>
-                    <a href={"#desc"}className={classes.arrow}>
+                    <a href={"#desc"} className={classes.arrow} onClick={click}>
                         <img src={leftPart} className={classes.arrow_part} alt={"Стрелка"}/>
                         <img src={rightPart} className={classes.arrow_part + " " + classes.rigth_part} alt={"Стрелка"}/>
                     </a>
